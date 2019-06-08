054bd0b12876b827c0be8062160ceb09fb0fde83
Aluizio Tomazelli Junior <aluiziotomazelli@gmail.com>
2019-06-08 03:12:41 -0400

    1

--- Equipamento/Equipamento.h
+++ Equipamento/Equipamento.h
@@ -14,32 +14,40 @@ class Equipamento {
 private:
     byte pino;                          //pino do equipamento
     bool ligado;                        //estado do pino
-    static Equipamento *atual;          //ponteiro para o ultimo objeto criado, compartilhado por todos objetos
+    static Equipamento *ultimo;         //ponteiro para o ultimo objeto criado, compartilhado por todos objetos
     Equipamento *anterior;              //ponteiro para o objeto anterior criado, cada objeto tem o seu
 
 public:
-    Equipamento(byte pin);
     Equipamento(byte pin, bool state);
     void setup();                       //Faz o setup do equipamento
     void liga();                        //Liga equipamento
     void desliga();                     //Desliga equipamento
     bool isLigado();                    //Informa se o equipamento está ligado
-
     static void setupAll();             //Chama setup para todos objetos criados
     static void desligaAll();           //Chama desliga para todos objetos
     static void ligaAll();              //Chama desliga para todos objetos
 
 };
-
-Equipamento *Equipamento::atual = 0;    //Inicia a variável estática em zero
+Equipamento *Equipamento::ultimo = 0;    //Inicia a variável estática em zero
 
 //  Construtor
 //  @param byte pin - pino da saída digital na qual o equipamento está ligado
 //  @param bool state - estado inicial da saída, HIGH ou LOW (true ou false, 1 ou 0)
+//
+//  No primeiro objeto criado, a sua variável "anterior" será 0, e a variável "ultimo"
+//      será um ponteiro para o próprio objeto. No segundo objeto criado, a sua variável
+//      anterior, será um ponteiro para o objeto que foi criado antes dele (no caso o primeiro),
+//      e a variável "ultimo", static, portanto compartilhada por todos objetos, será um
+//      ponteiro para o objeto atual. Dessa forma podemos "rastrear" todos objetos criados,
+//      pois a variável "ultimo" aponta sempre para o último objeto, e dentro dele há um ponteiro
+//      para o objeto anterior, e assim sucessivamente, até o primeiro objeto onde o ponteiro
+//      da variável "anterior é igual a zero. Com isso é possível chamar o mesmo método para todos
+//      objetos criados, partindo do último até chegar ao primeiro -- isso será usado nas funções
+//      setupAll() e demais xxxxxAll().
 Equipamento::Equipamento(byte pin, bool state)
         : pino(pin), ligado(state) {
-    anterior = atual;       //Copia para a variável "anterior" um ponteiro para o ultimo objeto criado
-    atual = this;           //Copia na variavel estática um ponteiro para o objeto que está sendo criado
+    anterior = ultimo;       //Copia para a variável "anterior" o ponteiro do ultimo objeto criado
+    ultimo = this;           //Copia na variável atual (static) um ponteiro para o objeto que está sendo criado
 }
 
 void Equipamento::setup() {
@@ -70,23 +78,29 @@ bool Equipamento::isLigado()
 
 //  setupAll() - Chama a função setup() para todos objetos criados
 //
-//  Faz um loop pegando o ponteiro para o ultimo objeto criado, chama setup para ele,
-//  troca para o objeto anterior e repete, até chegar no primeiro obejto criado, onde
-//  a variável "i" será 0 e irá interromper o laço.
+//  Faz um loop usando como controle a variável "ultimo", que aponta para o último
+//      objeto criado, e chama o método setup() para esse último objeto. Ao final
+//      da primeira execução do laço a variável de controle assume o valor da variável
+//      "anterior", que, no último objeto, aponta para o penúltimo objeto criado.
+//      Executa o laço novamente, que irá chamar setup() para o penúltimo objeto,
+//      onde, por sua vez, a variável "anterior" aponta para o antepenúltimo objeto criado.
+//      O laço é executado até que atinja o primeiro objeto criado, onde sua variável
+//      "anterior" tem valor 0, o que irá interromper o laço por não mais satisfazer
+//      a condição de controle: i != 0
 void Equipamento::setupAll() {
-    for (Equipamento *i = atual; i != 0; i = i->anterior)
+    for (Equipamento *i = ultimo; i != 0; i = i->anterior)
         i->setup();
 }
 
 //  desligaAll() - Chama a função desliga() para todos objetos criados
 void Equipamento::desligaAll() {
-    for (Equipamento *i = atual; i != 0; i = i->anterior)
+    for (Equipamento *i = ultimo; i != 0; i = i->anterior)
         i->desliga();
 }
 
 //  ligaAll() - Chama a função liga() para todos objetos criados
 void Equipamento::ligaAll() {
-    for (Equipamento *i = atual; i != 0; i = i->anterior)
+    for (Equipamento *i = ultimo; i != 0; i = i->anterior)
         i->liga();
 }
 
